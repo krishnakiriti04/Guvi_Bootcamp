4d6c8c8f59fd4ee82bcc2fb79ca116e985325c8a
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-08-07 21:36:05 +0530

    Create FactorialOfaNumber.js

---
+++ FactorialOfaNumber.js
@@ -0,0 +1,32 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+  console.log(factorial(+userInput[0]))
+  //end-here
+});
+
+
+function factorial(number)
+{
+    if (number === 1 || number === 0)
+        return 1;
+    else
+        return (number * factorial(number-1));
+    
+    
+}
+
+
