2b27a523c81603c237c959f3265f90ca19b3da01
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-08-08 00:10:15 +0530

    Create CircumferenceOfCircle.js

---
+++ CircumferenceOfCircle.js
@@ -0,0 +1,21 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+  var circumference = 2 * Math.PI * +userInput[0];
+  console.log(circumference.toFixed(2));
+
+  //end-here
+});
