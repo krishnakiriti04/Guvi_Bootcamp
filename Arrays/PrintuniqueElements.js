f481463c4fd024dae0217aa834ec3454fab52e84
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-08-18 19:08:14 +0530

    Create PrintuniqueElements.js

---
+++ Arrays/PrintuniqueElements.js
@@ -0,0 +1,37 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+  var len = parseInt(userInput[0]);
+  var arr = userInput[1].split(" ").map(Number);
+  const res = arr
+        .filter(value=>{
+              var count=0;
+              for(var i=0;i<len;i++)
+              {
+                  if(arr[i]===value)
+                    count++;
+              }
+              return count===2;
+          });
+        
+  console.log(res.join(" "));
+
+  //end-here
+});
+
+
+
+
