f147253be1828d731561a4f17e206ddc9556e322
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-09-04 15:56:59 +0530

    zen class day-2 task

---
+++ Zen-Tasks/Day-2 Task.js
@@ -0,0 +1,16 @@
+
+
+//----------------Task-3--------------
+console.log(typeof(1),
+typeof(1.1),
+typeof("1.1"),
+typeof(true),
+typeof(null),
+typeof(undefined),
+typeof([]),
+typeof({}))
+
+/*
+output:
+number number string boolean object undefined object object
+*/
