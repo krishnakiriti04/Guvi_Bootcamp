b3200ee66f403f493d58ee95942fb69e42fc43db
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-09-04 21:15:23 +0530

    Update Day-2 Task.js

--- Zen-Tasks/Day-2 Task.js
+++ Zen-Tasks/Day-2 Task.js
@@ -1,6 +1,12 @@
+//----------Task-0---------- deployed a test app in heroku using heroku CLI
+https://zenclass-day2-task.herokuapp.com/
+
+//-----------Task-1----------
 
 
-//----------------Task-3--------------
+//-----------Task-2----------
+
+//-----------Task-3----------
 console.log(typeof(1),
 typeof(1.1),
 typeof("1.1"),
