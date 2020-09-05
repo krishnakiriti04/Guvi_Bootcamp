82e1c7b63210ee7a5ce3496a08b699b63b9d37b8
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-09-05 08:55:27 +0530

    Update Day-2 Task.js

--- Zen-Tasks/Day-2 Task.js
+++ Zen-Tasks/Day-2 Task.js
@@ -1,10 +1,13 @@
-//----------Task-0---------- deployed a test app in heroku using heroku CLI
+//----------Task-0---------- 
+//deployed a test app in heroku using heroku CLI
 https://zenclass-day2-task.herokuapp.com/
 
 //-----------Task-1----------
+//html tags cheatsheet
 https://github.com/krishnakiriti04/Guvi_Bootcamp/blob/master/Zen-Tasks/Day-2/docs/htmltags.html
 
 //-----------Task-2----------
+//Resume html page
 https://github.com/krishnakiriti04/Guvi_Bootcamp/tree/master/Zen-Tasks/Day-2/docs/index.html
 https://github.com/krishnakiriti04/Guvi_Bootcamp/tree/master/Zen-Tasks/Day-2/docs/resume.css
 
@@ -22,3 +25,8 @@ typeof({}))
 output:
 number number string boolean object undefined object object
 */
+
+
+//-----------Task-4----------
+//objects and its internal representation
+https://github.com/krishnakiriti04/Guvi_Bootcamp/blob/master/Zen-Tasks/Day-2/docs/objects.txt
