dab55dbacfff26673e7ab549dd29afeef4a42636
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-09-05 16:29:18 +0530

    Update Day-2 Task.js

--- Zen-Tasks/Day-2 Task.js
+++ Zen-Tasks/Day-2 Task.js
@@ -4,12 +4,10 @@ https://zenclass-day2-task.herokuapp.com/
 
 //-----------Task-1----------
 //html tags cheatsheet
-https://github.com/krishnakiriti04/Guvi_Bootcamp/blob/master/Zen-Tasks/Day-2/docs/htmltags.html
+https://krishnakiriti04.github.io/Guvi-Tasks/HTML%20cheatsheet/
 
 //-----------Task-2----------
-//Resume html page
-https://github.com/krishnakiriti04/Guvi_Bootcamp/tree/master/Zen-Tasks/Day-2/docs/index.html
-https://github.com/krishnakiriti04/Guvi_Bootcamp/tree/master/Zen-Tasks/Day-2/docs/resume.css
+//Resume html pagehttps://krishnakiriti04.github.io/Guvi-Tasks/resume/
 
 //-----------Task-3----------
 console.log(typeof(1),
