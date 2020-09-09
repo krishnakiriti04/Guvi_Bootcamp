d5b28b81098c2b09a887e237dffc32ea6ebc70b8
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-09-09 20:53:35 +0530

    Add files via upload

---
+++ Zen-Tasks/Day-5 task.js
@@ -0,0 +1,40 @@
+
+/*Watch the Array methods 
+https://www.youtube.com/watch?v=R8rmfD9Y5-c
+*/
+
+//--------------------Task-1---------------------
+/*Do the below programs in arrow functions
+Print odd numbers in an array 
+Convert all the strings to title caps in a string array
+Sum of all numbers in an array
+Return all the prime numbers in an array
+Return all the palindromes in an array */
+
+//Link to JS file
+
+https://github.com/krishnakiriti04/Guvi_Bootcamp/blob/master/Zen-Tasks/Day-5/ArrowFunctions.js
+
+
+
+//--------------------Task-2---------------------
+/*
+Rest countries:
+Get the countries in asia - filter 
+Get the countries with population less than 2L - fliter
+Bumpup the current population  of all the counteirs by 1L - Map 
+convert all the names in capital - map
+print follwoig details  name , capital , flag  - foreach,filter
+Print the total population of the countries  - reduce
+Print the total population of the countries in asia - filter & reduce */
+
+//Link to JS file
+
+https://github.com/krishnakiriti04/Guvi_Bootcamp/blob/master/Zen-Tasks/Day-5/RestCountriesArrowFunctions.js
+
+
+//--------------------Task-3---------------------
+//https://docs.google.com/document/d/1AB04kg6FD9UuJgfW3P9Ml_Ol9tFCNJRVT2mx4jPKK3U/edit
+//Class 2: Circle- Class Diagram
+
+https://github.com/krishnakiriti04/Guvi_Bootcamp/blob/master/Zen-Tasks/Day-5/Classes.js
