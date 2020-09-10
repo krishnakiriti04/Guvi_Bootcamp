a674d3ab88a35967da533bc7c9f6c4607b8f5184
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-09-11 00:34:22 +0530

    Update Classes.js

--- Zen-Tasks/Day-5/Classes.js
+++ Zen-Tasks/Day-5/Classes.js
@@ -1,21 +1,17 @@
 //------------------------Calculator----------------------
 
-class calculator{
-	constructor(){
-	}
+class calculator {
+    constructor() {}
 
-	add(num1,num2)
-	{
-		console.log(num1+num2);
-	}
-	sub(num1,num2)
-	{
-		console.log(Math.absolute(num1-num2));
-	}
-	mul(num1,num2)
-	{
-		console.log(num1*num2);
-	}
+    add(num1, num2) {
+        console.log(num1 + num2);
+    }
+    sub(num1, num2) {
+        console.log(Math.absolute(num1 - num2));
+    }
+    mul(num1, num2) {
+        console.log(num1 * num2);
+    }
 }
 
 
@@ -27,74 +23,72 @@ var c = new calculator();
 
 //------------------------Movie----------------------
 
-class Movie{
-	constructor(title,studio,rating="PG"){
-		this.title = title;
-		this.studio = studio;
-		this.rating = rating;
-	}
+class Movie {
+    constructor(title, studio, rating = "PG") {
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
 
-	getPG(arr)
-	{
-		return arr.filter((val)=>val.rating==="PG");
-	}
+    getPG(arr) {
+        return arr.filter((val) => val.rating === "PG");
+    }
 }
 
 
-var movie1 = new Movie("Manam","Annapurna","A");
-var movie2 = new Movie("Darling","Ramanaidu","U")
-var movie3 = new Movie("Inside Out","Pixer")
-var movie4 = new Movie("Ready Player One","Disney")
+var movie1 = new Movie("Manam", "Annapurna", "A");
+var movie2 = new Movie("Darling", "Ramanaidu", "U")
+var movie3 = new Movie("Inside Out", "Pixer")
+var movie4 = new Movie("Ready Player One", "Disney")
 
 
-var moviearr = [movie1,movie2,movie3,movie4];
+var moviearr = [movie1, movie2, movie3, movie4];
 
-var hmovie = new Movie("Casino Royale","Eon Productions","PG13")
+var hmovie = new Movie("Casino Royale", "Eon Productions", "PG13")
 
 
 //console.log(hmovie.getPG(moviearr));
 
 
-//------------------------Movie----------------------
+//------------------------Circle----------------------
 
-class Circle{
-	constructor(radius=1.0, color="Red")
-	{
-		this.radius = radius;
-		this.color = color;
-	}
+class Circle {
+    constructor(...args) //radius=1.0, color="Red")
+        {
+            if (args.length == 0) {
 
-	getRadius(){
-		return this.radius;
-	}
+            } else if (args.length == 1) {
+                this.radius = args[0];
+            } else if (args.length == 2) {
+                this.radius = args[0];
+                this.color = args[1];
+            }
+        }
 
-	getColor(){
-		return this.color;
-	}
+    getRadius() {
+        return this.radius;
+    }
 
-	setRadius(num)
-	{
-		this.radius = num;
-	}
+    getColor() {
+        return this.color;
+    }
 
-	setColor(col){
-		this.color = col;
-	}
+    setRadius(num) {
+        this.radius = num;
+    }
 
-	getArea()
-	{
-		return Math.pow(this.radius,2).toFixed(2);
-	}
+    setColor(col) {
+        this.color = col;
+    }
 
-	toString()
-	{
-
-	}
+    getArea() {
+        return Math.pow(this.radius, 2).toFixed(2);
+    }
 
 }
 
 
-var circle = new Circle(5.4,"blue");
+var circle = new Circle(5.4);
 
 
 console.log(circle.getArea());
